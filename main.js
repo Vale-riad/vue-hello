dbe542f03db23f63b6224de3380c2274aefa0be1
Vale-riad <valentinariad91@gmail.com>
2022-11-11 16:33:34 +0100

    Aggiunge immagine

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 
 const {createApp} = Vue
 
-app=createApp({
+createApp({
     data() {
         return{
             message: 'ciao a tutti!',
             image: 'https://cdn-media-1.freecodecamp.org/ghost/2019/03/vueart.png',
+            cssClassList: 'center',
+            imageSize: 'size',
+            
         }
     }
 }).mount('#app')
